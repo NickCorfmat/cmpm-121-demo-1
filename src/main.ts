b07561b1c4db7c9a2856f7efb899e4b1d3b6b974
Nick Corfmat <ncorfmat@ucsc.edu>
2024-10-08 18:03:36 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "💩";
+app.append(button);
+
+button.addEventListener("click", () => {
+  console.log("button clicked");
+});
